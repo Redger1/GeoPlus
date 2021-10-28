cc2ae4b69e8fac9cabac66f3e393eb5085ce5041
Redger1 <artyom-sh2001@yandex.ru>
2021-10-28 18:30:11 +0300

    burger and scroll

--- js/script.js
+++ js/script.js
@@ -22,3 +22,50 @@ $(document).ready(function() {
 		variableWidth:false,
 	});
 });
+
+
+//1) Бургер
+//2) Кнопки(анимация)
+//3) Поп-апы
+//4) Страница компании
+//5) Страница Новости
+
+// Бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.header__navbar');
+if (iconMenu) {
+	const rightImage = document.querySelector('.right-side_inner');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('_lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth",
+			});
+			e.preventDefault();
+		}
+	}
+}
